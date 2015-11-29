585a6dde347347fe6e92d4091bfcd15b784b85e2
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-11-29 19:29:58 +0200

    Added Query interface base methods

--- src/orm/QueryInterface.php
+++ src/orm/QueryInterface.php
@@ -10,5 +10,48 @@ namespace samsonframework\orm;
  */
 interface QueryInterface
 {
+    /**
+     * Add condition to current query.
+     *
+     * @param string $fieldName Entity field name
+     * @param string $fieldValue Value
+     * @param string $relation Relation between field name and its value
+     * @return self Chaining
+     */
+    public function cond($fieldName, $fieldValue = null, $relation = '=');
 
+    /**
+     * Execute current query and receive collection of field values for RecordInterface collection
+     * received from database.
+     *
+     * @param string $columnName Database entity field name
+     * @param null|RecordInterface[] $return If variable is passed resulting collection would be
+     *                                      stored in this variable.
+     * @return bool|RecordInterface If method is called with $return parameter then then bool
+     *                                  with query result status would be returned, otherwise
+     *                                  query result collection would be returned.
+     */
+    public function fields($columnName, &$return = null);
+
+    /**
+     * Execute current query and receive collection of RecordInterface objects from database.
+     *
+     * @param null|RecordInterface[] $return If variable is passed resulting collection would be
+     *                                      stored in this variable.
+     * @return bool|RecordInterface If method is called with $return parameter then then bool
+     *                                  with query result status would be returned, otherwise
+     *                                  query result collection would be returned.
+     */
+    public function exec(&$return = null);
+
+    /**
+     * Execute current query and receive RecordInterface object from database.
+     *
+     * @param null|RecordInterface $return If variable is passed resulting RecordInterface would be
+     *                                      stored in this variable.
+     * @return bool|RecordInterface If method is called with $return parameter then then bool
+     *                                  with query result status would be returned, otherwise
+     *                                  query result RecordInterface would be returned.
+     */
+    public function first(&$return = null);
 }
---
+++ src/orm/RecordInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VITALYIEGOROV
+ * Date: 29.11.15
+ * Time: 19:19
+ */
+namespace samsonframework\orm;
+
+
+class RecordInterface
+{
+
+}
