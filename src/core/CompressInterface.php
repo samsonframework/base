4358b2d485583a070c84469f39b9353f82ea7168
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-11-29 20:04:16 +0200

    Added compress interface

---
+++ src/core/CompressInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Maxim Omelchenko <omelchenko@samsonos.com>
+ * on 03.04.2015 at 15:43
+ */
+namespace samsonframework\core;
+
+/**
+ * Module compressing interface
+ * @package samsonframework\core
+ * @author Vitaly Iegorov <egorov@samsonos.com>
+ */
+interface CompressInterface
+{
+
+}
