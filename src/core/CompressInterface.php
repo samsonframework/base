61690fb4bab25441be5afafc8a14ae818ca90a31
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-01-12 18:47:20 +0200

    Added compressinterface method stubs.

--- src/core/CompressInterface.php
+++ src/core/CompressInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by Maxim Omelchenko <omelchenko@samsonos.com>
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
  * on 03.04.2015 at 15:43
  */
 namespace samsonframework\core;
@@ -13,5 +13,21 @@ namespace samsonframework\core;
  */
 interface CompressInterface
 {
+    /**
+     * This method should be used to override generic compression logic.
+     *
+     * @param mixed $obj Pointer to compressor instance
+     * @param array|null $code Collection of already compressed code
+     * @return bool False if generic compression needs to be avoided
+     */
+    public function beforeCompress(&$obj = null, array &$code = null);
 
+    /**
+     * This method is called after generic compression logic has finished.
+     *
+     * @param mixed $obj Pointer to compressor instance
+     * @param array|null $code Collection of already compressed code
+     * @return bool False if generic compression needs to be avoided
+     */
+    public function afterCompress(&$obj = null, array &$code = null);
 }
