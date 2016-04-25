9f07cb3ae8f4dddd258d1de4a6c011fe9cbf33d1
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-04-25 12:37:53 +0300

    Added LoadableInterface which extends PreparableInterface and should be used in classes that can be loaded into framework

---
+++ src/core/LoadableInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>.
+ * on 25.04.16 at 12:13
+ */
+namespace samsonframework\core;
+
+/**
+ * Interface for SamsonFramework class loading.
+ *
+ * @author Vitaly Iegorov <egorov@samsonos.com>
+ * @package samsonframework\core
+ */
+interface LoadableInterface extends PreparableInterface
+{
+
+}
