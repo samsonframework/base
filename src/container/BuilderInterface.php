ab658183d8572c124af9d2ce08ca13a912c68128
nik-os <nick.w2r@gmail.com>
2016-08-22 18:11:46 +0300

    Add container builder intarface

---
+++ src/container/BuilderInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 03.04.2015 at 15:43
+ */
+namespace samsonframework\container;
+
+/**
+ * Container builder interface.
+ *
+ * @package samsonframework\container
+ * @author Vitaly Iegorov <egorov@samsonos.com>
+ */
+interface BuilderInterface
+{
+    /**
+     * Build container class.
+     *
+     * @param array $classMetadata Collection of classes metadata for container
+     * @param string|null $containerClass Container class name
+     * @param string      $namespace      Name space
+     *
+     * @return string Generated Container class code
+     * @throws \InvalidArgumentException
+     */
+    public function build(array $classesMetadata, $containerClass = 'Container', $namespace = '');
+
+}
