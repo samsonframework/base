31922554da70cfd66b70002bde5a6881aa3fe2c8
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-25 22:23:21 +0300

    Adde methods

--- src/i18n/i18nInterface.php
+++ src/i18n/i18nInterface.php
@@ -1,5 +1,31 @@
-<?php
+<?php declare(strict_types=1);
 namespace samsonframework\i18n;
 
-interface i18nInterface {
+/**
+ * Internalization(i18n) interface.
+ *
+ * @author  Vitaly Egorov <egorov@samsonos.com>
+ */
+interface I18nInterface
+{
+    /**
+     * Translate key with pluralization.
+     *
+     * @param string      $key Translatable key
+     * @param int         $count Key count
+     * @param string|null $locale Translation locale
+     *
+     * @return string Translated and pluralized key
+     */
+    public function plural(string $key, int $count, string $locale = null) : string;
+
+    /**
+     * Translate key.
+     *
+     * @param string      $key Translatable key
+     * @param string|null $locale Translation locale
+     *
+     * @return string Translated key
+     */
+    public function translate(string $key, string $locale = null) : string;
 }
